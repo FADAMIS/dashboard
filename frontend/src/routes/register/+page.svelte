4277aa057b1f2ba0d78c0183fa0aadb43d141baa
Adam Ragac <adam.ragac@outlook.com>
2023-11-26 20:52:22 +0100

    quick fix

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -29,7 +29,7 @@
                 surname: surname,
                 email: email,
                 telefon:  telefon,
-                id: ParseInt(id)
+                id: parseInt(id)
 
             })
         })
