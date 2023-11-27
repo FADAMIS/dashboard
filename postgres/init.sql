a815bd6b030aaba96aae13214f58f93b2c4c67dd
Fabucik <fanda.burkon@seznam.cz>
2023-11-27 14:07:53 +0100

    Set sample camp to open registration

--- postgres/init.sql
+++ postgres/init.sql
@@ -22,4 +22,4 @@ INSERT INTO foods(name) VALUES('none');
 -- DEFAULT USER admin WITH PASSWORD admin. CHANGE THE PASSWORD!!!
 INSERT INTO admins(username, password) VALUES('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
 
-INSERT INTO camps(name, date, processed) VALUES('sample', 0, true);
+INSERT INTO camps(name, date, processed) VALUES('sample', 0, false);
