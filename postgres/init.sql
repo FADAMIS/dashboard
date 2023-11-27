a4e5be3beeca5458b804e7ba48c67c092440a99e
Fabucik <fanda.burkon@seznam.cz>
2023-11-27 13:55:05 +0100

    add default values in init.sql

--- postgres/init.sql
+++ postgres/init.sql
@@ -4,4 +4,22 @@ CREATE TABLE foods(
     imagepath varchar(250)
 );
 
+CREATE TABLE admins(
+    id serial PRIMARY KEY,
+    username varchar (50) NOT NULL,
+    password varchar(64) NOT NULL
+);
+
+CREATE TABLE camps(
+    id serial PRIMARY KEY,
+    name varchar (50) NOT NULL,
+    date bigint,
+    processed boolean
+);
+
 INSERT INTO foods(name) VALUES('none');
+
+-- DEFAULT USER admin WITH PASSWORD admin. CHANGE THE PASSWORD!!!
+INSERT INTO admins(username, password) VALUES('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
+
+INSERT INTO camps(name, date, processed) VALUES('sample', 0, true);
